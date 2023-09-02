4c4eb1145312438712e6b38fb4e8ca36b9b5aea7
Less Wright <lessw@etrillium.com>
2023-09-02 11:33:19 -0700

    add get_slopes, credit original authors

--- alibi_embeddings.py
+++ alibi_embeddings.py
@@ -1,5 +1,10 @@
 # derived in part from LucidRains impl:
-https://github.com/lucidrains/x-transformers/tree/main
+# https://github.com/lucidrains/x-transformers/tree/main
+# and the inventor / source of alibi:
+# https://github.com/ofirpress/attention_with_linear_biases
+
+import torch
+import torch.nn as nn
 
 
 class Alibi(nn.Module):
@@ -25,3 +30,35 @@ class Alibi(nn.Module):
 
         bias = -torch.abs(j_range - i_range)
         return bias
+
+    @staticmethod
+    def get_slopes(num_heads):
+        def get_slopes_power_of_2(n):
+            start = (2**(-2**-(math.log2(n)-3)))
+            ratio = start
+            return [start*ratio**i for i in range(n)]
+
+        if math.log2(num_heads).is_integer():
+            return get_slopes_power_of_2(num_heads)
+            
+        # paper authors note they only trained models that have 2^a heads for some a.  
+        # This has beneficial properties related to input being power of 2. 
+        # Closest power of 2 below is workaround for when num of heads is not power of 2
+        
+        closest_power_of_2 = 2 ** math.floor(math.log2(num_heads))
+        return get_slopes_power_of_2(closest_power_of_2) + get_slopes_power_of_2(2 * closest_power_of_2)[0::2][:num_heads-closest_power_of_2]
+
+    # original alibi
+    def get_slopes(n):
+            def get_slopes_power_of_2(n):
+                start = (2**(-2**-(math.log2(n)-3)))
+                ratio = start
+                return [start*ratio**i for i in range(n)]
+
+            if math.log2(n).is_integer():
+                return get_slopes_power_of_2(n)                   #In the paper, we only train models that have 2^a heads for some a. This function has
+            else:                                                 #some good properties that only occur when the input is a power of 2. To maintain that even
+                closest_power_of_2 = 2**math.floor(math.log2(n))  #when the number of heads is not a power of 2, we use this workaround. 
+                return get_slopes_power_of_2(closest_power_of_2) + get_slopes(2*closest_power_of_2)[0::2][:n-closest_power_of_2]
+ 
+
