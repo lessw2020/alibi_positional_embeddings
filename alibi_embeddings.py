3d5c9faca664ebf12b9962dfa2d9dcb31c59ec8e
Less Wright <lessw@etrillium.com>
2023-09-07 07:35:19 -0700

    Update usage, causal mask

--- alibi_embeddings.py
+++ alibi_embeddings.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
 import math
+from typing import List
 
 
 class AlibiPE(nn.Module):
@@ -38,8 +39,8 @@ class AlibiPE(nn.Module):
             alibi_mask = self.alibi.get_attention_mask(N) # N = seq length of this batch
             ...
             attn = q @ k.transpose( -2, -1)
-            attn *= 1.0 / math.sqrt(k.size(-1))
-            attn += alibi_mask
+            att *= 1.0 / math.sqrt(k.size(-1))
+            att += alibi_mask
 
         """
         super().__init__()
@@ -54,25 +55,31 @@ class AlibiPE(nn.Module):
         self.decoder_mask = self.causal_mask + self.alibi_mask_base
         self.register_buffer("alibi_mask", self.decoder_mask, persistent=False)
 
-    def get_attention_mask(self, curr_seq_len: int) -> torch.tensor:
+    def get_attention_mask(self, curr_seq_len: int) -> torch.Tensor:
         """returns the alibi mask, clipped to the current batch seq len"""
-        return self.alibi_mask[:, :curr_seq_len, :curr_seq_len]
+        return self.alibi_mask[..., :curr_seq_len, :curr_seq_len]
 
-    def build_causal_attention_mask(self, seq_len: int, num_heads: int) -> torch.Tensor:
+    @classmethod
+    def build_causal_attention_mask(cls, seq_len: int, num_heads: int) -> torch.Tensor:
         """builds a generic causal attention mask"""
-        causal_mask = torch.ones(seq_len, seq_len).tril()
-        causal_mask = causal_mask.masked_fill(causal_mask == 0, -float("inf"))
+        causal_mask = torch.triu(
+            torch.ones(seq_len, seq_len) * float("-inf"), diagonal=1
+        )
         attn_mask = causal_mask.repeat(num_heads, 1, 1)
         return attn_mask
 
-    def build_alibi_mask(self, seq_len: int, num_heads: int) -> torch.Tensor:
-        """generate the alibi mask by computing a distance matrix multiplied by each head's m (slope)"""
-        distance_matrix = torch.arange(seq_len) - torch.arange(seq_len).view(-1, 1)
-        slope_per_head = Tensor(self.get_slopes(num_heads)).view(-1, 1, 1)
-        alibi_mask = distance_matrix * slope_per_head
+    @classmethod
+    def build_alibi_mask(cls, seq_len: int, num_heads: int) -> torch.Tensor:
+        """generate the alibi mask by computing a distance bias matrix multiplied by each head's m (slope)"""
+        distance_bias_matrix = -torch.abs(
+            torch.arange(seq_len) - torch.arange(seq_len).view(-1, 1)
+        )
+        slope_per_head = Tensor(cls.get_slopes(num_heads)).view(-1, 1, 1)
+        alibi_mask = distance_bias_matrix * slope_per_head
         return alibi_mask
 
-    def get_slopes(self, num_heads: int) -> torch.Tensor:
+    @staticmethod
+    def get_slopes(num_heads: int) -> List[float]:
         """for n heads, a range from (0,1) and is the geometric sequence
         that starts at 2^(-8/n) and uses this same value as its ratio
 
@@ -81,7 +88,7 @@ class AlibiPE(nn.Module):
 
         """
 
-        def get_slopes_power_of_2(n):
+        def get_slopes_power_of_2(n: int) -> List[float]:
             start = 2 ** (-(2 ** -(math.log2(n) - 3)))
             ratio = start
             return [start * ratio**i for i in range(n)]
@@ -89,7 +96,7 @@ class AlibiPE(nn.Module):
         if math.log2(num_heads).is_integer():
             return get_slopes_power_of_2(num_heads)
 
-        # paper authors note they only trained models that have 2^a heads for some a.
+        # paper authors note that they only trained models that have 2^a heads for some a.
         # This has beneficial properties related to input being power of 2.
         # Closest power of 2 below is workaround for when num of heads is not power of 2
 
